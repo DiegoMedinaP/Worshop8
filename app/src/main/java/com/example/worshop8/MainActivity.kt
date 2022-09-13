fb4f46c5db0444ef61ca3f4a48304d23736ac5e6
vn51b44 <diego.medina0@walmart.com>
2022-09-13 11:28:13 -0500

    hola mundo cambios

--- app/src/main/java/com/example/worshop8/MainActivity.kt
+++ app/src/main/java/com/example/worshop8/MainActivity.kt
@@ -7,5 +7,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val holaMUndo = ""
+        val holaMUndo1 = ""
+        val holaMUndo2 = ""
+        val holaMUndo3 = ""
     }
 }
